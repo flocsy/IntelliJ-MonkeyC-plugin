9ec0f000d5bfc077a5281cacb9b2394a25114696
GitHub Action <action@github.com>
2021-07-21 20:35:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/flocsy/intellijmonkeycplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.flocsy.intellijmonkeycplugin.services
+
+import com.github.flocsy.intellijmonkeycplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/flocsy/intellijmonkeycplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/flocsy/intellijmonkeycplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.flocsy.intellijmonkeycplugin.services
 
+import com.github.flocsy.intellijmonkeycplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
